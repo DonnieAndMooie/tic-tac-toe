b419eb7c119d7a1b92c1a2ecf4a9f91306f7c35e
DonnieAndMooie <owenjeaves@gmail.com>
2022-11-30 11:37:55 +0000

    Display contents of array on a grid on screen

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const gameBoard = () => {
-    const board = [ , , ,
-                    , , ,
-                    , , ,]
+    const board = ["X","O","O",
+                   "O","X","O",
+                   "O","X","X"]
     return board
 }
 
-const displayController = () => {
-
-}
+const displayController = (() => {
+    const updateBoard = () => {
+        const squares = document.querySelectorAll(".square");
+        const currentBoard = gameBoard()
+        for (const square of squares){
+            let index = square.getAttribute("index")
+            square.textContent = currentBoard[index]
+        }
+    }
+    return {updateBoard}})()
 
 const Player = () => {
 
 }
+
+displayController.updateBoard()
