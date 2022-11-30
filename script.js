b8465397c49b8ca2798689b0bc00cce9a08ab241
DonnieAndMooie <owenjeaves@gmail.com>
2022-11-30 10:57:05 +0000

    Create modules and factory functions for gameBoard, displayController, player

---
+++ script.js
@@ -0,0 +1,14 @@
+const gameBoard = () => {
+    const board = [ , , ,
+                    , , ,
+                    , , ,]
+    return board
+}
+
+const displayController = () => {
+
+}
+
+const Player = () => {
+
+}
