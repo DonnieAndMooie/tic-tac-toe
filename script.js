6767a9f794a33597cfeb6a440b426ac4391f4de8
DonnieAndMooie <owenjeaves@gmail.com>
2022-11-30 14:50:53 +0000

    Allow the user to click in a square and draw marker

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-const gameBoard = () => {
-    const board = ["X","O","O",
-                   "O","X","O",
-                   "O","X","X"]
-    return board
-}
-
 const displayController = (() => {
-    const updateBoard = () => {
-        const squares = document.querySelectorAll(".square");
-        const currentBoard = gameBoard()
+    const squares = document.querySelectorAll(".square");
+    const updateGrid = () => {
+        const currentBoard = gameBoard.board
         for (const square of squares){
             let index = square.getAttribute("index")
             square.textContent = currentBoard[index]
         }
     }
-    return {updateBoard}})()
 
-const Player = () => {
+    return {updateGrid}})()
 
+
+const gameBoard = (() => {
+    let board = ["","","",
+                 "","","",
+                 "","",""]
+    const updateBoard = (marker1, marker2) => {
+        let currentMarker = marker1
+        const squares = document.querySelectorAll(".square");
+        displayController.updateGrid()
+        for (const square of squares){
+            index = square.getAttribute("index")
+            square.addEventListener("click", (e) => {
+                let index = e.target.getAttribute("index")
+                if (board[index] === ""){
+                    board[index] = currentMarker
+                    if (currentMarker === marker1){
+                        currentMarker = marker2
+                    }
+                    else{
+                        currentMarker = marker1
+                    }
+                    displayController.updateGrid()
+                }})
+            
+            
+    }
+    return board
+}
+    return {board, updateBoard}
+})()
+
+const Player = (marker) => {
+    return {marker}
 }
 
-displayController.updateBoard()
+const player1 = Player("X")
+const player2 = Player("O")
+gameBoard.updateBoard(player1.marker, player2.marker)
