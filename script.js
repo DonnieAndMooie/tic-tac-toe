bd9133d0adaa0aa93fcb937e3516002c4caeca93
DonnieAndMooie <owenjeaves@gmail.com>
2022-12-07 14:48:01 +0000

    Add option to play against impossible AI using minimax

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+let currentMarker;
 
     const displayController = (() => {
         const squares = document.querySelectorAll(".square");
         const updateGrid = () => {
-            const currentBoard = gameBoard.board
+            const currentBoard = gameBoard.board;
             for (const square of squares){
-                let index = square.getAttribute("index")
-                square.textContent = currentBoard[index]
+                let index = square.getAttribute("index");
+                square.textContent = currentBoard[index];
             }
         }
 
@@ -15,64 +16,184 @@
     const gameBoard = (() => {
         let board = ["","","",
                      "","","",
-                     "","",""]
+                     "","",""];
         const updateBoard = (marker1, marker2, name1, name2) => {
-            startBtn.textContent = "Restart"
-            let currentMarker = marker1
+            startBtn.textContent = "Restart";
             const squares = document.querySelectorAll(".square");
-            displayController.updateGrid()
+            displayController.updateGrid();
             for (const square of squares){
-                index = square.getAttribute("index")
+                index = square.getAttribute("index");
                 square.addEventListener("click", (e) => {
-                    let index = e.target.getAttribute("index")
+                    let index = e.target.getAttribute("index");
                     if (board[index] === ""){
-                        board[index] = currentMarker
+                        board[index] = currentMarker;
                         if (currentMarker === marker1){
-                            currentMarker = marker2
+                            currentMarker = marker2;
                         }
                         else{
-                            currentMarker = marker1
+                            currentMarker = marker1;
                         }
-                        displayController.updateGrid()
-                        let gameOver = game.checkGameOver(name1, name2)
-                        if (gameOver === true){
-                            currentMarker = null
+                        displayController.updateGrid();
+                        let gameOver = game.checkGameOver(name1, name2, board)
+                        if (gameOver === 10 || gameOver === -10 || gameOver === 0){
+                            currentMarker = "";
                         }
                     }})
                 
                 
         }
-        return board
+        return board;
     }
 
     const updateBoardVsAI = (marker1, marker2, name1, name2) => {
-        aiStartBtn.textContent = "Restart"
+        aiStartBtn.textContent = "Restart";
         const squares = document.querySelectorAll(".square");
-        displayController.updateGrid()
+        displayController.updateGrid();
         for (const square of squares){
-            index = square.getAttribute("index")
+            index = square.getAttribute("index");
             square.addEventListener("click", (e) => {
-                let index = e.target.getAttribute("index")
+                let index = e.target.getAttribute("index");
                 if (board[index] === ""){
-                    board[index] = marker1
-                    let randomChoice = Math.floor((Math.random() * 9));
-                    while (board[randomChoice] !== ""){
-                        randomChoice = Math.floor((Math.random() * 9));
-                    }
-                    board[randomChoice] = marker2
-                    }
+                    board[index] = marker1;
+                    displayController.updateGrid();
+                    let gameOver = game.checkGameOver(name1, name2, board);
+                    if (gameOver !== 10 && gameOver !== -10 && gameOver !== 0){
+                        let randomChoice = Math.floor((Math.random() * 9));
+                        while (board[randomChoice] !== ""){
+                            randomChoice = Math.floor((Math.random() * 9));
+                        }
+                        board[randomChoice] = marker2;
+                        }}
                     displayController.updateGrid()
-                    let gameOver = game.checkGameOver(name1, name2)
-                    if (gameOver === true){
-                        currentMarker = null
+                    let gameOver = game.checkGameOver(name1, name2, board);
+                    if (gameOver === 10 || gameOver === -10 || gameOver === 0){
+                        marker1 = "";
+                        marker2 = "";
                     }
                 })
             }}
+
+        const updateBoardvsHardAI = (marker1, marker2, name1, name2) => {
+            aiStartBtnHard.textContent = "Restart";
+            const squares = document.querySelectorAll(".square");
+            displayController.updateGrid();
+            for (const square of squares){
+                index = square.getAttribute("index");
+                square.addEventListener("click", (e) => {
+                    let index = e.target.getAttribute("index");
+                    if (board[index] === ""){
+                        board[index] = currentMarker;
+                        displayController.updateGrid();
+                        let gameOver = game.checkGameOver(name1, name2, board);
+                        if (gameOver !== 10 && gameOver !== -10 && gameOver !== 0){
+                            let AIChoice = minimax(board, marker2).index;
+                            board[AIChoice] = marker2;
+                            }
+                            }
+                        displayController.updateGrid();
+                        let gameOver = game.checkGameOver(name1, name2, board);
+                        if (gameOver === 10 || gameOver === -10 || gameOver === 0){
+                            currentMarker = null;
+                        }
+                    })
+                }}
+
+            const findAvailableSquares = (currentBoard) =>{
+                let availableSquares = [];
+                let squareIndex = -1;
+                for (const square of currentBoard){
+                    squareIndex++;
+                    if (square !== "X" && square !== "O"){
+                        availableSquares.push(squareIndex);
+                    }
+                }
+                return availableSquares;
+            }
+
+            const minimax = (currentBoard, marker) => {
+                let newBoard = convertBoard(currentBoard);
+
+                let availableSpots = findAvailableSquares(newBoard);
+                if (game.winning(newBoard, "X")){
+                    return {score:-10};
+                }
+                else if(game.winning(newBoard, "O")){
+                    return {score: 10};
+                }
+                else if (availableSpots.length === 0){
+                    return {score:0};
+                }
+                
+                let moves = [];
+                for (let i=0; i<availableSpots.length; i++){
+
+                    outputDiv.textContent = "";
+                    let move = {};
+                    move.index = newBoard[availableSpots[i]];
+
+                    newBoard[availableSpots[i]] = marker;
+
+                    if (marker === "O"){
+                        let result = minimax(newBoard, "X");
+                            move.score = result.score;
+                        }
+                        
+                    
+                    else{
+                        let result = minimax(newBoard, "O");
+                            move.score = result.score;
+                        }
+                    
+                    newBoard[availableSpots[i]] = move.index;
+                    
+                    moves.push(move);
+                    }
+                    
+    
+            
+
+                let bestMove;
+                if (marker === "O"){
+                    let bestScore = -100000;
+                    for (let i = 0; i < moves.length; i++){
+                        if (moves[i].score > bestScore){
+                            bestScore = moves[i].score;
+                            bestMove = i;
+                        }}
+                    }
+                else{
+                    let bestScore = 100000;
+                    for (let i = 0; i < moves.length; i++){
+                        if (moves[i].score < bestScore){
+                            bestScore = moves[i].score;
+                            bestMove = i;
+                        }}
+                    }
+                
+                
+                return moves[bestMove];
+                }
+
+            const convertBoard = (currentBoard) => {
+                let newBoard = [];
+                let boardIndex = -1;
+                for (let i = 0; i < currentBoard.length; i++){
+                    boardIndex++;
+                    if (currentBoard[i] === "X" || currentBoard[i] === "O"){
+                        newBoard.push(currentBoard[i]);
+                    }
+                    else{
+                        newBoard.push(boardIndex);
+                    }
+                    
+            }
+            return newBoard;
+        }
                 
             
     
 
-        return {board, updateBoard, updateBoardVsAI}
+        return {board, updateBoard, updateBoardVsAI, updateBoardvsHardAI, findAvailableSquares, minimax}
         })()
 
     const Player = (marker, name) => {
@@ -80,135 +201,183 @@
     }
 
     const game = (() => {
-        const checkGameOver = (name1, name2) => {
-            const outputDiv = document.querySelector(".output-winner")
-            const currentBoard = gameBoard.board
-            if (currentBoard[0] === currentBoard[1] 
+        const checkGameOver = (name1, name2, board) => {
+            const outputDiv = document.querySelector(".output-winner");
+            const currentBoard = board;
+            if (currentBoard[0] === currentBoard[1]
                 && currentBoard[1] === currentBoard[2]
                 && currentBoard[0] !== ""){
                     if (currentBoard[0] ==="X"){
-                        outputDiv.textContent = name1 + " WINS!"}
+                        outputDiv.textContent = name1 + " WINS!";
+                        return -10}
                     else{
-                        outputDiv.textContent = name2 + " WINS!"
+                        outputDiv.textContent = name2 + " WINS!";
+                        return 10
                     }
-                return true
             }
             else if (currentBoard[3] === currentBoard[4] 
                 && currentBoard[4] === currentBoard[5]
                 && currentBoard[3] !== ""){
                     if (currentBoard[3] ==="X"){
-                        outputDiv.textContent = name1 + " WINS!"}
+                        outputDiv.textContent = name1 + " WINS!";
+                        return -10}
+
+                    
                     else{
-                        outputDiv.textContent = name2 + " WINS!"
-                    }
-                return true
-            }
+                        outputDiv.textContent = name2 + " WINS!";
+                        return 10
+                    }}
+            
             else if (currentBoard[6] === currentBoard[7] 
                 && currentBoard[7] === currentBoard[8]
                 && currentBoard[6] !== ""){
                     if (currentBoard[6] ==="X"){
-                        outputDiv.textContent = name1 + " WINS!"}
-                    else{
-                        outputDiv.textContent = name2 + " WINS!"
+                        outputDiv.textContent = name1 + " WINS!";
+                        return -10
                     }
-                return true
+                    else{
+                        outputDiv.textContent = name2 + " WINS!";
+                        return 10
+                    }
+
             }
             else if (currentBoard[0] === currentBoard[3] 
                 && currentBoard[3] === currentBoard[6]
                 && currentBoard[0] !== ""){
                     if (currentBoard[0] ==="X"){
-                        outputDiv.textContent = name1 + " WINS!"}
-                    else{
-                        outputDiv.textContent = name2 + " WINS!"
+                        outputDiv.textContent = name1 + " WINS!";
+                        return -10
                     }
-                return true
+                    else{
+                        outputDiv.textContent = name2 + " WINS!";
+                        return 10
+                    }
+
             }
             else if (currentBoard[1] === currentBoard[4] 
                 && currentBoard[4] === currentBoard[7]
                 && currentBoard[1] !== ""){
                     if (currentBoard[1] ==="X"){
-                        outputDiv.textContent = name1 + " WINS!"}
-                    else{
-                        outputDiv.textContent = name2 + " WINS!"
+                        outputDiv.textContent = name1 + " WINS!";
+                        return -10
                     }
-                return true
+                    else{
+                        outputDiv.textContent = name2 + " WINS!";
+                        return 10
+                    }
             }
             else if (currentBoard[2] === currentBoard[5] 
                 && currentBoard[5] === currentBoard[8]
                 && currentBoard[2] !== ""){
                     if (currentBoard[2] ==="X"){
-                        outputDiv.textContent = name1 + " WINS!"}
+                        outputDiv.textContent = name1 + " WINS!";
+                        return -10
+                }
                     else{
-                        outputDiv.textContent = name2 + " WINS!"
+                        outputDiv.textContent = name2 + " WINS!";
+                        return 10
                     }
-                return true
             }
             else if (currentBoard[0] === currentBoard[4] 
                 && currentBoard[4] === currentBoard[8]
                 && currentBoard[0] !== ""){
                     if (currentBoard[0] ==="X"){
-                        outputDiv.textContent = name1 + " WINS!"}
-                    else{
-                        outputDiv.textContent = name2 + " WINS!"
+                        outputDiv.textContent = name1 + " WINS!";
+                        return -10
                     }
-                return true
+                    else{
+                        outputDiv.textContent = name2 + " WINS!";
+                        return 10
+                    }
             }
             else if (currentBoard[2] === currentBoard[4] 
                 && currentBoard[4] === currentBoard[6]
                 && currentBoard[2] !== ""){
                     if (currentBoard[2] ==="X"){
-                        outputDiv.textContent = name1 + " WINS!"}
-                    else{
-                        outputDiv.textContent = name2 + " WINS!"
+                        outputDiv.textContent = name1 + " WINS!";
+                        return -10
                     }
-                return true
+                    else{
+                        outputDiv.textContent = name2 + " WINS!";
+                        return 10
+                    }
             }
             else{
                 for (let i=0; i<9; i++){
                     if (currentBoard[i] === ""){
-                        outputDiv.textContent = ""
-                        return
-                    }
-                    else{
-                        outputDiv.textContent = "IT'S A DRAW!"
+                        outputDiv.textContent = "";
+                        return false;
                     }
                     
                 }
+                outputDiv.textContent = "IT'S A DRAW!";
+                return 0;
                 
             }
-            return
+            
             
         }
-        return {checkGameOver}
+
+        const winning = (board, marker) => {
+            if (
+                (board[0] === marker && board[1] === marker && board[2] === marker)||
+                (board[3] === marker && board[4] === marker && board[5] === marker)||
+                (board[6] === marker && board[7] === marker && board[8] === marker)||
+                (board[0] === marker && board[3] === marker && board[6] === marker)||
+                (board[1] === marker && board[4] === marker && board[7] === marker)||
+                (board[2] === marker && board[5] === marker && board[8] === marker)||
+                (board[0] === marker && board[4] === marker && board[8] === marker)||
+                (board[2] === marker && board[4] === marker && board[6] === marker)
+                ){
+                    return true
+                }
+                else {
+                    return false
+                }
+        }
+
+        return {checkGameOver, winning}
     })()
 
-const startBtn = document.querySelector(".start")
-const aiStartBtn = document.querySelector(".ai-start")
-const player1Name = document.getElementById("player1")
-const player2Name = document.getElementById("player2")
-const outputDiv = document.querySelector(".output-winner")
+const startBtn = document.querySelector(".start");
+const aiStartBtn = document.querySelector(".ai-start");
+const aiStartBtnHard = document.querySelector(".ai-start-hard");
+const player1Name = document.getElementById("player1");
+const player2Name = document.getElementById("player2");
+const outputDiv = document.querySelector(".output-winner");
 const squares = document.querySelectorAll(".square");
+
 
 
 startBtn.addEventListener("click", () => {
     if (player1Name.value === "" || player2Name.value === ""){
     }
     else{     
-    event.preventDefault()
-    const player1 = Player("X", player1Name.value)
-    const player2 = Player("O", player2Name.value)
-    gameBoard.board.fill("")
-    outputDiv.textContent = ""
-    gameBoard.updateBoard(player1.marker, player2.marker, player1.name, player2.name)
+    event.preventDefault();
+    const player1 = Player("X", player1Name.value);
+    const player2 = Player("O", player2Name.value);
+    gameBoard.board.fill("");
+    outputDiv.textContent = "";
+    currentMarker = player1.marker;
+    gameBoard.updateBoard(player1.marker, player2.marker, player1.name, player2.name);
     }})
     
 
 aiStartBtn.addEventListener("click", () => {
-    const player1 = Player("X", player1Name.value)
-    const player2 = Player("O", "AI")
-    event.preventDefault()
-    gameBoard.board.fill("")
-    outputDiv.textContent = ""
-    gameBoard.updateBoardVsAI(player1.marker, player2.marker, player1.name, "AI")
+    const player1 = Player("X", "You");
+    const player2 = Player("O", "AI");
+    gameBoard.board.fill("");
+    outputDiv.textContent = "";
+    currentMarker = player1.marker;
+    gameBoard.updateBoardVsAI(player1.marker, player2.marker, player1.name, "AI");
 
+})
+
+aiStartBtnHard.addEventListener("click", () => {
+    const player1 = Player("X", "You");
+    const player2 = Player("O", "AI");
+    gameBoard.board.fill("");
+    currentMarker = player1.marker;
+    gameBoard.updateBoardvsHardAI(player1.marker, player2.marker, player1.name, player2.name);
+    outputDiv.textContent = "";
 })
